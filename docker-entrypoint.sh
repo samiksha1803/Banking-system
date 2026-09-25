dbe1367ed89e5f279d71bc3e8a942229784bde67
Samiksha <samikshadesai37@gmail.com>
2026-09-26 00:54:12 +0530

    Fix Tomcat PORT binding for Render health checks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
+
 PORT="${PORT:-8080}"
-sed -i "s/port=\"8080\"/port=\"${PORT}\"/" /usr/local/tomcat/conf/server.xml
-exec catalina.sh run
+CONF=/usr/local/tomcat/conf/server.xml
+
+# Shutdown port must not accept HTTP; Render health checks were hitting it (HEAD / warnings).
+sed -i 's/port="8005"/port="-1"/' "$CONF"
+
+# HTTP connector must listen on Render's PORT (not 8080).
+if grep -q 'port="8080" protocol="HTTP/1.1"' "$CONF"; then
+  sed -i "s|port=\"8080\" protocol=\"HTTP/1.1\"|port=\"${PORT}\" protocol=\"HTTP/1.1\" address=\"0.0.0.0\"|" "$CONF"
+else
+  sed -i "s|protocol=\"HTTP/1.1\" port=\"8080\"|protocol=\"HTTP/1.1\" port=\"${PORT}\" address=\"0.0.0.0\"|" "$CONF"
+fi
+
+exec /usr/local/tomcat/bin/catalina.sh run
